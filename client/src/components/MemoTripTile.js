d16fa20b3f206f68fb683b2c0de4f114f4edcf88
Raddi Hansen <raddji@gmail.com>
2022-06-03 12:11:48 -0400

    format date string at memo trips index page

--- client/src/components/MemoTripTile.js
+++ client/src/components/MemoTripTile.js
@@ -2,7 +2,9 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 const MemoTripTile = (props) => {
+
   const { name, where, when, what, pic, id } = props.memoTrip
+  
   return (
     <div className="flex-tiles">
       <div className="card index-card">
@@ -11,7 +13,7 @@ const MemoTripTile = (props) => {
             <li className="memory-name-color">{name}</li>
             </Link>
             <li>{where}</li>
-            <li>{when}</li>
+            <li>{when.substring(0, 10)}</li>
             <li>{what}</li>
           </ul>  
         </div>
